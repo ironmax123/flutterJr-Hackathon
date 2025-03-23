869f29b219a7743d2229ca6e741791deb704d71b
eita <>
2025-03-23 15:34:57 +0900

    add: sample用mian作成

---
+++ lib/sample_main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:room_check/feature/sample/view.dart';
+import 'package:room_check/main.dart';
+
+Future<void> main() async {
+  await mainCommonMethod();
+  runApp(
+    const ProviderScope(child: MyApp()),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const SampleScreen(),
+    );
+  }
+}
