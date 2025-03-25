6a769b0ecc8b4170fa4dd776f4dce9de7639b5c0
TOMOKI SUDO <136161374+TOMU-74@users.noreply.github.com>
2025-03-25 23:47:17 +0900

    add:ホームのスクロール作成

--- lib/feature/posts/view.dart
+++ lib/feature/posts/view.dart
@@ -1,4 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:room_check/primary/components/user_icon.dart';
+import 'package:room_check/primary/utils/color.dart';
 
 class PostsScreen extends StatelessWidget {
   const PostsScreen({super.key});
@@ -7,9 +11,31 @@ class PostsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Posts Screen')),
-      body: const Center(
-        child: Text('Posts Screen'),
-      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              PrimaryUserIcon(
+                onTap: () {},
+                imageUrl: 'null',
+                ),
+          
+              Center(
+                child: CachedNetworkImage(
+                  width: 340,
+                  imageUrl: 'https://images.unsplash.com/photo-1515513284006-9a59075694b7?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                ),
+              ),
+              const Gap(14),
+              const Divider(color: AppColor.dividerColor),
+            ],
+          ),
+        );
+      }),
     );
   }
 }
