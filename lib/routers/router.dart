f066be1a5e144129b8011f629412461fe29161cf
eita <>
2025-03-27 00:04:26 +0900

    fix: ルーティング

--- lib/routers/router.dart
+++ lib/routers/router.dart
@@ -3,11 +3,44 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:room_check/feature/home/view.dart';
 import 'package:room_check/feature/invitation/view.dart';
+import 'package:room_check/feature/login/page.dart';
+import 'package:room_check/feature/login_check.dart';
+import 'package:room_check/feature/sign_up/page.dart';
 
 GoRouter goRouter(List<CameraDescription> cameras) {
   return GoRouter(
-    initialLocation: '/home', // Set a valid initial location
+    initialLocation: '/feature', // Set a valid initial location
     routes: [
+      GoRoute(
+        path: '/feature',
+        name: 'LoginCheckPage',
+        pageBuilder: (context, state) {
+          return MaterialPage(
+            key: state.pageKey,
+            child: const LoginCheckPage(),
+          );
+        },
+      ),
+      GoRoute(
+        path: '/login',
+        name: 'LoginScreen',
+        pageBuilder: (context, state) {
+          return MaterialPage(
+            key: state.pageKey,
+            child: const LoginPage(),
+          );
+        },
+      ),
+      GoRoute(
+        path: '/sing_up',
+        name: 'SignupScreen',
+        pageBuilder: (context, state) {
+          return MaterialPage(
+            key: state.pageKey,
+            child: const SignupPage(),
+          );
+        },
+      ),
       GoRoute(
         path: '/home',
         name: 'Home',
