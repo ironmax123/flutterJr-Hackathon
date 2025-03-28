a4437642cfc5b0d72f6ff64d393f12ca10fbd832
eita <>
2025-03-28 14:59:13 +0900

    fix: 不用なimport削除

--- lib/feature/userposts/view.dart
+++ lib/feature/userposts/view.dart
@@ -1,15 +1,11 @@
-import 'dart:developer';
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:room_check/data/post/entity.dart';
-import 'package:room_check/feature/posts/vm.dart';
 import 'package:room_check/feature/userposts/components/user_only_info.dart';
 import 'package:room_check/feature/userposts/vm.dart';
-import 'package:room_check/primary/components/user_icon.dart';
 import 'package:room_check/primary/utils/color.dart';
 import 'package:room_check/supabase/supabase.dart';
 import 'package:room_check/utils/timeago.dart';
